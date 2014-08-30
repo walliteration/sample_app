317d3561f8d3905c01f645f40b22caf1427a853e
Walter Wong <walter.chong.wong@gmail.com>
2014-08-30 20:21:35 +0800

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-  
+	before_action :signed_in_user, 	only: [:index, :edit, :update, :destroy]  
+	before_action :correct_user,	only: [:edit, :update]
+	before_action :admin_user,		only: :destroy
+
+	def index
+		@users = User.paginate(page: params[:page])
+	end
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -19,11 +26,53 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		# @user = User.find(params[:id])
+		# why was this removed after before_action :correct_user was added?
+	end
+
+	def update
+		# @user = User.find(params[:id])
+		# this too.
+		# Tutorial says "Now that the correct_user before filter defines @user, we can omit it"
+		if @user.update_attributes(user_params)
+			flash[:success] = "Profile updated"
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User deleted."
+		redirect_to users_url
+	end
+
 	private
 	# Why does the test fail when I comment this section out?
 	# It says 'param' method undefined. How is that related to user_params?
 	# What about the 'params[:id]' section above?
 		def user_params # Was this automatically generated then modified, or completely new?
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
+
+		# Before filters
+
+		def signed_in_user
+			unless signed_in?
+				store_location
+				redirect_to signin_url, notice: "Please sign in."
+			# redirect_to signin_url, notice: "Please sign in." unless signed_in?
+			end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_url) unless current_user?(@user)
+		end
+
+		def admin_user
+			redirect_to(root_url) unless current_user.admin?
 		end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
 	factory :user do # :user symbol tells Factory Girl it's for a User model object
-		name 		"Michael Hartl"
-		email		"michael@example.com"
+		# name 		"Michael Hartl"
+		# email		"michael@example.com"
+		sequence(:name)		{ |n| "Person #{n}" }
+		sequence(:email)	{ |n| "person_#{n}@example.com" }
 		password	"foobar"
 		password_confirmation	"foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
